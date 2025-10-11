f8005266d1dd8a0907618e9336b46f4184d91e75
Yuri12005 <yurii.butvynnyk@gmail.com>
2025-10-11 23:08:49 +0300

    Database connected

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using Npgsql;
+using DotNetEnv;
+
+namespace AdoNetPostgresDemo
+{
+    internal class Program
+    {
+        static string connectionString;
+
+        static void Main(string[] args)
+        {
+            Env.Load();
+            connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            Console.WriteLine("✅ Підключення успішне!");
+        }
+    }
+}
